053351d3fe15fe5f9eeef048c25153c9f278c84a
бону <qudratovamohtabonu@gmail.com>
2025-03-13 16:18:15 +0500

    Add quiz logic

---
+++ index.js
@@ -0,0 +1,56 @@
+const number1Elm = document.querySelector('#number1')
+const number2Elm = document.querySelector('#number2')
+const operationElm = document.querySelector('#operation')
+const answerBtns = document.querySelectorAll('button')
+
+function generateQuestion () {
+  let number1 = Math.floor(Math.random() * 100)
+  let number2 = Math.floor(Math.random() * 100)
+  let operations = ['+', '-', '*']
+  let operation = operations[Math.floor(Math.random() * operations.length)]
+
+  let correctAnswer = eval(`${number1} ${operation} ${number2}`)
+  let answers = [correctAnswer]
+
+  while (answers.length < 4) {
+    let randomAnswer = correctAnswer + Math.floor(Math.random() * 20) - 10
+    if (!answers.includes(randomAnswer)) answers.push(randomAnswer)
+  }
+
+  answers.sort(() => Math.random() - 0.5)
+  return { number1, number2, operation, correctAnswer, answers }
+}
+
+function renderQuiz (question) {
+  number1Elm.innerText = question.number1
+  number2Elm.innerText = question.number2
+  operationElm.innerText = question.operation
+
+  answerBtns.forEach((btn, i) => {
+    btn.innerText = question.answers[i]
+    btn.onclick = () => checkAnswer(btn, question.answers[i], question.correctAnswer)
+  })
+}
+
+function checkAnswer(btn, selectedAnswer, correctAnswer) {
+     
+    if(selectedAnswer === correctAnswer) btn.classList.add('green')
+    else btn.classList.add('red')
+  
+    setTimeout(() => {
+     if(btn.classList.contains('green')) btn.classList.remove('green')
+     else btn.classList.remove('red')
+      init();
+    }, 1000)
+}
+  
+
+
+const questions = [];
+function init () {
+  let question = generateQuestion()
+  questions.push(question);
+  renderQuiz(question)
+}
+
+init()
